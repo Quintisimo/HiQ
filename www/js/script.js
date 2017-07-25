5c65d4247b167f7c0f3286512265e12b0a2fe5b0
Quintisimo <quintuscardozo13@gmail.com>
2017-07-25 13:31:47 +1000

    Changed on hover event to on click event and added transitions

--- www/js/script.js
+++ www/js/script.js
@@ -1,15 +1,14 @@
 (function($) {
   $(document).ready(function() {
-    $('a').mouseover(function(){
+    $('a').click(function(){
       var $this = $(this),
       className = 'section.' + $this.attr('class');
       $('section.games').css('display', 'none');
+      $this.addClass('is-selected');
       $('body').find(className).css('display', 'block');
-    }).mouseout(function() {
-      var $this = $(this),
-      className = 'section.' + $this.attr('class');
       setTimeout(function() {
         $('body').find(className).css('display', 'none');
+        $this.removeClass('is-selected');
       }, 5000);
     });
   });
