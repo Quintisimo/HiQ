e6b2aa7411c84c129395dca2db986853ec4ac7d8
Quintisimo <quintuscardozo13@gmail.com>
2017-07-26 23:55:06 +1000

    Changed background

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,12 @@
 (function($) {
   $(document).ready(function() {
-    $('a').click(function(){
+    $('img').click(function(){
       var $this = $(this),
       className = 'section.' + $this.attr('class');
       $('section.games').fadeOut();
-      $('a').removeClass('is-selected');
-      $this.addClass('is-selected');
       $('body').find(className).fadeIn();
       setTimeout(function() {
         $('body').find(className).fadeOut();
-        $this.removeClass('is-selected');
       }, 420000);
     });
   });
