2bb0b0ab011afef1dccc255aa1002cac197cf90f
Quintisimo <quintuscardozo13@gmail.com>
2017-07-27 23:50:10 +1000

    Added animations

--- public/js/script.js
+++ public/js/script.js
@@ -3,11 +3,14 @@
     $('a').click(function(){
       var $this = $(this),
       className = 'section.' + $this.attr('class');
+      $('a').removeClass('is-selected');
       $('section').fadeOut();
+      $this.addClass('is-selected');
       $('body').find(className).fadeIn();
       setTimeout(function() {
           $('section.games').fadeOut();
           $('section.qutesports').fadeIn();
+          $this.removeClass('is-selected');
       }, 420000);
     });
   });
